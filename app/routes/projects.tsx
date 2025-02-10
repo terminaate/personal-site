95feb6b981fc09d66d0e67b071bef92673ac3287
terminaate <tntwnik@gmail.com>
2025-02-10 23:52:16 +0300

    Add temp version of projects

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,18 +1,30 @@
 import type { MetaFunction } from '@remix-run/node';
 import { BasicPage } from '@/components/BasicPage';
 import cl from '#/projects.module.scss';
+import { projects } from '@/common/constants/projects';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Terminaate | Projects' }];
 };
 
+// const ProjectCard = () => {};
+
 export default function Projects() {
   return (
     <BasicPage className={cl.page}>
-      This page is under construction ;)
-      {/*{projects.map((project, index) => (*/}
-      {/*  <ProjectCard className={cl.projectCard} key={index} project={project} />*/}
-      {/*))}*/}
+      {projects.map((project, index) => (
+        <div key={index} className={cl.projectCard}>
+          <img
+            className={cl.projectImage}
+            src={project.image}
+            alt={project.title}
+          />
+          <div className={cl.projectInfoContainer}>
+            <h2>{project.title}</h2>
+            <span>{project.description}</span>
+          </div>
+        </div>
+      ))}
     </BasicPage>
   );
 }
