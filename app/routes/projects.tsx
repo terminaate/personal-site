ff5719f4f3c7db87562a524edafb83d27a6956d3
Terminaate <terminaatecorp@gmail.com>
2025-04-15 14:34:46 +0300

    Integrate a basic Todo component as a placeholder for the projects page.

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from '@remix-run/node';
 import { BasicPage } from '@/components/BasicPage';
 import cl from '#/projects.module.scss';
 import { projects } from '@/common/constants/projects';
+import { Todo } from '@/components/Todo';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Terminaate | Projects' }];
@@ -10,6 +11,7 @@ export const meta: MetaFunction = () => {
 export default function Projects() {
   return (
     <BasicPage className={cl.page}>
+      <Todo>// TODO: redesign this page (but rn im out of ideas)</Todo>
       {projects.map((project, index) => (
         <a
           style={{ backgroundImage: `url(${project.image})` }}
