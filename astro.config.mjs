d4186cba551fab5954b3873ecaf6c86fb455bf8d
terminaate <terminaatecorp@gmail.com>
2025-08-27 19:28:34 +0300

    add sitemap, add expressiveCode and enable decapCmsOauth only in prod environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,37 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
-
 import vercel from '@astrojs/vercel';
-
 import decapCmsOauth from 'astro-decap-cms-oauth';
+import expressiveCode from 'astro-expressive-code';
+import sitemap from '@astrojs/sitemap';
+
+const IS_DEV = process.env.NODE_ENV === 'development';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://terminaate.site',
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark-high-contrast',
     },
-    rehypePlugins: [rehypeAccessibleEmojis],
   },
 
   integrations: [
-    mdx(),
     icon(),
-    decapCmsOauth({
-      decapCMSSrcUrl: 'https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js',
-    }),
+    ...(IS_DEV
+      ? []
+      : [
+          decapCmsOauth({
+            decapCMSSrcUrl:
+              'https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js',
+          }),
+        ]),
+    expressiveCode(),
+    sitemap(),
+    mdx(),
   ],
 
   adapter: vercel(),
